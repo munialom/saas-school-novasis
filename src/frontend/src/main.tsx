048c88dfe830447bdb47acea54bfddf8374b7fcc
munialom <munialom@gmail.com>
2024-12-30 00:08:49 +0300

    appstate in main

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { RouterProvider } from 'react-router-dom';
 import AppLoader from './components/common/AppLoader';
 import 'antd/dist/reset.css';
 import router from "./router";
-
+import { AppStateProvider } from './context/AppState';
 import AuthProvider from "./context/AuthContext";
 
 
@@ -16,12 +16,12 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
     <React.StrictMode>
-
+        <AppStateProvider>
             <AuthProvider>
                 <React.Suspense fallback={<AppLoader />}>
                     <RouterProvider router={router} />
                 </React.Suspense>
             </AuthProvider>
-
+        </AppStateProvider>
     </React.StrictMode>
 );
